06f8ff9e0b55fc8ae58280bcd049f38266fc1ab9
Luca Manuel De Veintemilla Altamirano <lmde2@espe.edu.ec>
2024-02-01 07:19:40 -0500

    Creacion

--- librerialocal/settings.py
+++ librerialocal/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'librerialocal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pruebassoftware',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
